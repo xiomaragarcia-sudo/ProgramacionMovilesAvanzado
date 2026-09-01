326520e178f20f6ad591be7d102265fb21c23b6a
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 22:51:06 -0500

    ejercicio1.3

---
+++ Semana01/Lab01-IntroSwift/Ejercicio1.3.swift
@@ -0,0 +1,22 @@
+// PREDICT 1: 30
+// PREDICT 2: Hola Mundo
+// PREDICT 3: 3
+// PREDICT 4: Double
+let x = 10
+var y = 20
+y = y + x
+print(y) // PREDICT 1
+ 
+let a = "Hola"
+let b = "Mundo"
+print(a + " " + b) // PREDICT 2
+ 
+var contador = 0
+contador = contador + 1
+contador = contador + 1
+contador = contador + 1
+print(contador) // PREDICT 3
+ 
+let entero = 7
+let decimal = Double(entero)
+print(type(of: decimal)) // PREDICT 4
