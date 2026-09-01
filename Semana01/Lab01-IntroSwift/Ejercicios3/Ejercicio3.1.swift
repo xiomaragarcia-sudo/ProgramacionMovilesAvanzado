0a90353243a566185f99638d1db82521abe3da3a
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 23:00:16 -0500

    ejercicio 3.1

---
+++ Semana01/Lab01-IntroSwift/Ejercicios3/Ejercicio3.1.swift
@@ -0,0 +1,10 @@
+let nombre = "Xiomara"
+let apellido = "Garcia"
+let edad = 22
+let carrera = "Diseño y Desarrollo de Software"
+let ciclo = "V"
+let distrito = "Puente Piedra"
+ 
+print("Me llamo \(nombre) \(apellido), tengo \(edad) años.")
+print("Estudio \(carrera), ciclo \(ciclo).")
+print("Vivo en \(distrito).")
