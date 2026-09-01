420870b1bb2ea3865ccbc189974545b392adffcc
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 22:47:47 -0500

    ejercicio01

---
+++ Semana01/Lab01-IntroSwift/Ejercicio01.swift
@@ -0,0 +1,21 @@
+import UIKit
+// ===== EJERCICIO 1: VARIABLES Y CONSTANTES =====
+//Docente: Juan León
+ 
+// --- TODO 1: Declara una CONSTANTE con tu nombre completo ---
+let nombreCompleto = "Xiomara Mercedes"
+ 
+// --- TODO 2: Declara una VARIABLE con tu edad ---
+var edad = 22
+ 
+// --- TODO 3: Declara una CONSTANTE con tu estatura (Double) ---
+let estatura: Double = 1.51
+ 
+// --- TODO 4: Declara una VARIABLE Bool: ¿trabajas actualmente? ---
+var trabaja: Bool = false
+ 
+// --- TODO 5: Declara una CONSTANTE con tu número de celular como String ---
+let celular: String = "912261788"
+ 
+// --- TODO 6: Imprime TODAS tus variables con print() ---
+print("Estudiante: \(nombreCompleto), Edad: \(edad), Estatura: \(estatura), ¿Trabaja actualmente?: \(trabaja), Celular: \(celular)")
