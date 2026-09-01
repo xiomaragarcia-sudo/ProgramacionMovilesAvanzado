83a21a787d28601ed4801ef84f4d13da15138312
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 22:54:20 -0500

    ejercicio2.1

---
+++ Semana01/Lab01-IntroSwift/Ejercicios2/Ejercicio2.1.swift
@@ -0,0 +1,17 @@
+let a = 25
+let b = 7
+ 
+let suma = a + b
+print("Suma: \(a) + \(b) = \(suma)")
+ 
+let resta = a - b
+print("Resta: \(a) - \(b) = \(resta)")
+ 
+let multiplicacion = a * b
+print("Multiplicación: \(a) * \(b) = \(multiplicacion)")
+ 
+let divisionEntera = a / b
+print("División entera: \(a) / \(b) = \(divisionEntera)")
+ 
+let residuo = a % b
+print("Residuo: \(a) % \(b) = \(residuo)")
