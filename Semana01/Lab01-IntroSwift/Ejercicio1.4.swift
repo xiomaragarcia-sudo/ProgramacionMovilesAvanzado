293302a18c373e94ee6a98f59246a6995b1b9ea3
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 22:52:11 -0500

    ejercicio1.4

---
+++ Semana01/Lab01-IntroSwift/Ejercicio1.4.swift
@@ -0,0 +1,15 @@
+// TODO 7: Una variable con el valor 2026
+let anio = 2026
+print(type(of: anio)) // Int
+ 
+// TODO 8: Una variable con el valor 72.5
+let peso = 72.5
+print(type(of: peso)) // Double
+ 
+// TODO 9: Una variable con el valor "Lima"
+let ciudad = "Lima"
+print(type(of: ciudad)) // String
+ 
+// TODO 10: Una variable con el valor false
+let matriculado = false
+print(type(of: matriculado)) // Bool
