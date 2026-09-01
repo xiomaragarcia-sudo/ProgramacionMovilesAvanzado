a42b3be8be7862a9bbd8cace843fd5c7e10c717f
Xiomara Garcia <xiomara.garcia@tecsup.edu.pe>
2026-08-31 23:02:35 -0500

    ejercicio 3.3 swift

---
+++ Semana01/Lab01-IntroSwift/Ejercicios3/Ejercicio3.3.swift
@@ -0,0 +1,13 @@
+// 6a: "Swift versión 5"
+// 6b: "5 × 5 = 25"
+// 6c: "Hay 8 alumnos"
+// 6d: "Descuento: S/. 15.0"
+let n = "Swift"
+let v = 5
+print("\(n) versión \(v)")
+print("\(v) × \(v) = \(v * v)")
+print("Hay \(v + 3) alumnos")
+ 
+let p1 = 100.0
+let d = 0.15
+print("Descuento: S/. \(p1 * d)")
